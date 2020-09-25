c23de1906a9baf01be89abdb6a1f2dd422cd1024
Raymond MASSON <rmasson@vente-privee.com>
2020-09-25 22:40:50 +0200

    query experimentation

--- Repositories/AddressRepository.cs
+++ Repositories/AddressRepository.cs
@@ -3,6 +3,7 @@ using Entities;
 using Entities.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace Repositories
 {
@@ -15,8 +16,9 @@ namespace Repositories
 		}
 
 		public List<FullContact> GetFullContacts()
-		{
-			return this.RepositoryContext.Contacts.Join(
+		{              
+            // INNER JOIN
+			/*return this.RepositoryContext.Contacts.Join(
                 this.RepositoryContext.Addresses,
                 contact => contact.Id,
                 address => address.ContactId,
@@ -27,7 +29,40 @@ namespace Repositories
                     FirstName = contact.FirstName,
                     LastName = contact.LastName
                 }   
-            ).ToList();
+            ).ToList();*/
+
+            // LEFT JOIN
+            /*return this.RepositoryContext.Contacts
+                    .GroupJoin(
+                        this.RepositoryContext.Addresses,
+                        contact => contact.Id,
+                        address => address.ContactId,
+                        (contact, address) => new { contact, address }
+                    )
+                    .SelectMany(
+                        x => x.address.DefaultIfEmpty(), 
+                        (x, address) => new FullContact {
+                            FirstName = x.contact.FirstName,
+                            LastName = x.contact.LastName,
+                            City = address == null ? string.Empty : address.City,
+                            ZipCode = address == null ? 0 : address.ZipCode
+                        }
+                    ).ToList();*/
+
+            // Query syntax
+            var query = from contact in this.RepositoryContext.Contacts
+                              join address in this.RepositoryContext.Addresses
+                              on contact.Id equals address.ContactId
+                              into ContactAddressGroup
+                              from address in ContactAddressGroup.DefaultIfEmpty()
+                              select new FullContact
+                              {
+                                  FirstName = contact.FirstName,
+                                  LastName = contact.LastName,
+                                  ZipCode = address.ZipCode,
+                                  City = address.City,
+                              };
+            return query.ToList();
 		}
 	}
 }
